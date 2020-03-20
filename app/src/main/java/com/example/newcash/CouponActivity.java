26d91134cb121c5191c5f76ce4f3b22a365efd07
note1130 <note1130@gmail.com>
2020-03-20 17:16:29 +0900

    2020.03.20

---
+++ app/src/main/java/com/example/newcash/CouponActivity.java
@@ -0,0 +1,23 @@
+package com.example.newcash;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class CouponActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save);
+
+
+
+
+    }
+
+
+}
